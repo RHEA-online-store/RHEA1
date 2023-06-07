53b33ff5762d47599e56bb64209fc5064a014831
Lexus-it4 <118810198+Lexus-it4@users.noreply.github.com>
2023-06-07 15:47:43 +0300

    adding

--- src/main/java/com/rhea/store/repository/ProductCardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rhea.store.repository;
-
-import com.rhea.store.entity.ProductCard;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductCardRepository extends CrudRepository<ProductCard, Long> {
-}
